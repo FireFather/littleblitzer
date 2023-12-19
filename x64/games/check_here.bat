9c0cfc78c4631eb195e0f37f937c2451c933ba9c
Kato-Daichi <firefather@telenet.be>
2023-12-19 16:49:51 +0100

    update 2.76

--- x64/games/check_here.bat
+++ x64/games/check_here.bat
@@ -1,5 +1,5 @@
 echo off
-copy C:\littleblitzer\Results.pgn result.pgn
+copy C:\LittleBlitzer\Results.pgn result.pgn
 ordo-win64.exe -a 2300 -W -p result.pgn -F97 -s100 -j details.txt -o ordo.txt
 
 
--- x64/games/ordo.bat
+++ x64/games/ordo.bat
@@ -1,1 +1,1 @@
-ordo-win64.exe -a 3400 -W -p result.pgn -F97 -s100 -j details.txt -o ordo.txt -c ordo_csv.txt
+ordo-win64.exe -a 2300 -W -p result.pgn -F97 -s100 -j details.txt -o ordo.txt -c ordo_csv.txt
